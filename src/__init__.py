96cfbf4e9d1087b0f4557d0f08faca8bd32e308d
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-10-25 22:25:27 +0530

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+from src.auth import auth
+from src.bookmarks import bookmarks
+from src.database import db
+
+
+
+def create_app(test_config=None):
+    app = Flask(__name__,
+     instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False
+        )
+
+    else:
+        app.config.from_mapping(test_config)
+
+    db.app=app
+    db.init_app(app)
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
+
+
+
+    return app
