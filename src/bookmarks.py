ab136fda419e5091209fc84ab5e6956dcfd5daff
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-10-26 16:57:11 +0530

    added bookmarks route

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,8 +1,76 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask import json
+from flask.json import jsonify
+from flask_jwt_extended.view_decorators import jwt_required
+import validators
+from src.database import Bookmark, db
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 bookmarks = Blueprint("bookmarks",__name__,url_prefix="/api/v1/bookmarks")
 
-@bookmarks.get("/")
-def b():
-    return "bookmark page"
 
+@bookmarks.route('/', methods=['GET', 'POST'])
+@jwt_required()
+def handle_bookmarks():
+    current_user = get_jwt_identity()
+
+    if request.method == 'POST':
+        body = request.get_json().get('body', '')
+        url = request.get_json().get('url', '')
+
+        if not validators.url(url):
+            return jsonify({
+                'error':"Enter a valid url"
+            }), 400
+
+        if Bookmark.query.filter_by(url=url, user_id=current_user).first():
+            return jsonify({
+                'error':"URL already exists"
+            }), 409
+
+        bookmark = Bookmark(url=url, body=body, user_id=current_user)
+        db.session.add(bookmark)
+        db.session.commit()
+
+        return jsonify({
+            'id': bookmark.id,
+            'url': bookmark.url,
+            'short_url': bookmark.short_url,
+            'visit': bookmark.visits,
+            'body': bookmark.body,
+            'created_at': bookmark.created_at,
+            'updated_at': bookmark.updated_at
+        }), 201
+
+    elif request.method=='GET':
+
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+
+        bookmarks = Bookmark.query.filter_by(
+            user_id=current_user).paginate(page=page, per_page=per_page)
+
+        data = []
+
+        for bookmark in bookmarks.items:
+            data.append({
+                'id':bookmark.id,
+                'url':bookmark.url,
+                'short_url':bookmark.short_url,
+                'visit':bookmark.visits,
+                'body':bookmark.body,
+                'created_at':bookmark.created_at,
+                'updated_at':bookmark.updated_at
+            })
+
+            meta = {
+                'page': bookmarks.page,
+                'pages': bookmarks.pages,
+                'total_count': bookmarks.total,
+                'prev_page' : bookmarks.prev_num,
+                'next_page' : bookmarks.next_num,
+                'has_next' : bookmarks.has_next,
+                'has_prev' : bookmarks.has_prev
+            }
+
+        return jsonify({'data': data, 'meta':meta}), 200
--- src/database.py
+++ src/database.py
@@ -29,14 +29,14 @@ class Bookmark(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
 
-    def generare_short_characters(self):
+    def generate_short_characters(self):
         characters = string.digits+string.ascii_letters
         picked_chars = ''.join(random.choices(characters, k=3))
 
         link=self.query.filter_by(short_url=picked_chars).first()
 
         if link:
-            self.generare_short_characters()
+            self.generate_short_characters()
         else:
             return picked_chars
 
